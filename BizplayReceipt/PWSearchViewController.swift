4dc4ef4397ead79989ed62001f2fe4cb985b650d
juanagustin0504 <juanagustin@naver.com>
2019-12-16 17:48:50 +0900

    Add PWSearchViewController.swift

---
+++ BizplayReceipt/PWSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PWSearchViewController.swift
+//  BizplayReceipt
+//
+//  Created by Webcash on 2019/12/16.
+//  Copyright © 2019 WebCash. All rights reserved.
+//
+
+import UIKit
+
+class PWSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationItem.backBarButtonItem?.title = ""
+    }
+}
