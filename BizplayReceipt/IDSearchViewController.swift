05d60970df24b56a1a0cb8df1e3e5f43ed75cb0f
juanagustin0504 <juanagustin@naver.com>
2019-12-16 17:46:36 +0900

    Add IDSearchViewController.swift

---
+++ BizplayReceipt/IDSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IDSearchViewController.swift
+//  BizplayReceipt
+//
+//  Created by Webcash on 2019/12/16.
+//  Copyright © 2019 WebCash. All rights reserved.
+//
+
+import UIKit
+
+class IDSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationItem.backBarButtonItem?.title = ""
+    }
+
+}
