eb542458abac4f0be44d38fcc69980e05865e0bd
juanagustin0504 <juanagustin@naver.com>
2019-12-31 11:21:37 +0900

    Added EctReceiptPopup

---
+++ BizplayReceipt/Common/Popup/EctReceiptPopup.swift
@@ -0,0 +1,34 @@
+//
+//  EctReceiptPopup.swift
+//  BizplayReceipt
+//
+//  Created by Webcash on 2019/12/31.
+//  Copyright © 2019 WebCash. All rights reserved.
+//
+
+import UIKit
+
+class EctReceiptPopup: UIViewController {
+
+    @IBOutlet weak var btnCamera: UIButton!
+    @IBOutlet weak var btnNewRcpt: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
