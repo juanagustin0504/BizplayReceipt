cdbd15c6b202b6f9155284b039a9893a2ae64be8
juanagustin0504 <juanagustin@naver.com>
2020-01-02 17:39:52 +0900

    Added ReceiptTableViewCell UI and Identifier

---
+++ BizplayReceipt/View/MyReceipt/FillOutReceipt/ReceiptTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptTableViewCell.swift
+//  BizplayReceipt
+//
+//  Created by Webcash on 2020/01/02.
+//  Copyright © 2020 WebCash. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
